d2afc5fe7169494effb1ce3b244383fc6b29c13d
Quentin <quentin.solard@hotmail.fr>
2017-11-09 16:05:14 +0100

    je commit de bon matin

--- linear_regression.py
+++ linear_regression.py
@@ -24,8 +24,10 @@ inputs = pd.read_csv("data/biblio_3_per_half_hour.csv", sep=";")
 inputs['Date'] = pd.to_datetime(inputs['Date'], format='%d/%m/%Y %H:%M')
 inputs['weekday'] = inputs['Date'].map(lambda x: x.weekday())
 inputs["hours"] = inputs["Date"].dt.strftime("%H%M")
+inputs["Visiteurs presents"] = pd.to_numeric(inputs["Visiteurs presents"])
 # print(inputs["hours"])
 filtered_inputs = inputs[inputs["Visiteurs presents"] > 0]
+filtered_inputs = filtered_inputs[filtered_inputs["Date"].dt.hour == 14]
 (training_X, training_y, test_X, test_y) = separate(filtered_inputs)
 
 
@@ -35,14 +37,14 @@ filtered_inputs = inputs[inputs["Visiteurs presents"] > 0]
 # print(X)
 # print(separate_learn_and_test_data(X))
 
-def show_data(plot, test_X, test_y, title="", x_label="", y_label="", color="red", nbins=15):
+def show_data(plot, test_X, test_y, title="", x_label="", y_label="", color="red", nbins=20):
     # plt.figure()
     plt.subplot(plot)
     plt.scatter(test_X, test_y)
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.locator_params(axis="y", tight=False, nbins=nbins)
+    plt.locator_params(axis="y", tight=None, nbins=nbins)
     # plt.show()
 
 
