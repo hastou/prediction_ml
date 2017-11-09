ae681cb21bfb18360d68dc7db0794862b59d8d5a
Quentin <quentin.solard@hotmail.fr>
2017-11-09 15:10:51 +0100

    push

--- linear_regression.py
+++ linear_regression.py
@@ -16,15 +16,18 @@ def separate(inputs):
 
 
 def get_x_y(inputs):
-    x = inputs.loc[:, ["weekday"]]
+    x = inputs.loc[:, ["weekday", "hours"]]
     y = inputs.loc[:, ["Visiteurs presents"]]
     return (x, y)
 
 inputs = pd.read_csv("data/biblio_3_per_half_hour.csv", sep=";")
 inputs['Date'] = pd.to_datetime(inputs['Date'], format='%d/%m/%Y %H:%M')
 inputs['weekday'] = inputs['Date'].map(lambda x: x.weekday())
-(training_X, training_y, test_X, test_y) = separate(inputs)
-# filtered_inputs = inputs[inputs["Visiteurs presents"] > 0]
+inputs["hours"] = inputs["Date"].dt.strftime("%H%M")
+# print(inputs["hours"])
+filtered_inputs = inputs[inputs["Visiteurs presents"] > 0]
+(training_X, training_y, test_X, test_y) = separate(filtered_inputs)
+
 
 # X = filtered_inputs.loc[:, ["Date", "weekday"]]
 # y = filtered_inputs.loc[:, ["Visiteurs presents"]]
@@ -32,12 +35,29 @@ inputs['weekday'] = inputs['Date'].map(lambda x: x.weekday())
 # print(X)
 # print(separate_learn_and_test_data(X))
 
+def show_data(plot, test_X, test_y, title="", x_label="", y_label="", color="red"):
+    # plt.figure()
+    plt.subplot(plot)
+    plt.scatter(test_X, test_y)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    # plt.locator_params(numticks=15)
+    # plt.show()
+
+
 regressor.fit(training_X, training_y)
 y_pred = regressor.predict(test_X)
-plt.scatter(test_X, test_y, color="red")
-plt.plot((range(0, 7)), regressor.predict((range(0, 7))), color="blue")
-plt.title("Salary vs Experience (Training set")
-plt.xlabel("Years of experience")
-plt.ylabel("Salary")
+# plt.scatter(test_X["weekday"], test_y, color="red")
+# plt.scatter(test_X["hours"], test_y, color="orange")
+# plt.plot((range(0, 7)), regressor.predict((range(0, 7))), color="blue")
+# plt.title("Salary vs Experience (Training set")
+# plt.xlabel("Years of experience")
+# plt.ylabel("Salary")
+# plt.show()
+
+show_data(211, test_X["weekday"], test_y, title="Weekday", x_label="Weekday", y_label="Visteurs présents")
+show_data(212, test_X["hours"], test_y, title="Hours", x_label="Hours", y_label="Visiteurs présents")
 plt.show()
+
 
