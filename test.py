bbc8e7649e3a9ac6f58be131346c9a19a4a61b21
tbmc <me@tbmc.fr>
2017-12-13 02:21:55 +0100

    remove files, add zip to reduce size, etc.

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
 from glob import glob
-from os.path import join
 import pandas as pd
 from utils.model_testing import test_models, write_new_results
 
 
-path = join("data", "per_half_hour")
+path = "data/per_half_hour"
 
 
 def get_all_files():
     all = None
-    files = glob(join((path, "*.csv")))
+    files = glob(path + "/*.csv")
     csvs = []
     for file in files:
         csv = pd.read_csv(file, sep=";", decimal=",")
         csv = csv[csv.iloc[:, 1] != 0]
         csvs.append(csv)
     df = pd.concat(csvs, ignore_index=True)
+
+    holidays = pd.read_csv("data/holidays.csv", encoding="latin_1")
+    meteo = pd.read_csv("data/meteo_paris.zip")
+
     return df
 
 
+get_all_files()
 
